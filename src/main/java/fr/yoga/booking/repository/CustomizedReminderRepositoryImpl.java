d8f8e36b6f76099f23bc4891f0a9d22747b07dca
Aurélien Baudet <aurelien.baudet@gmail.com>
2020-10-24 18:38:15 +0400

    fix(cast) Compilation error

--- src/main/java/fr/yoga/booking/repository/CustomizedReminderRepositoryImpl.java
+++ src/main/java/fr/yoga/booking/repository/CustomizedReminderRepositoryImpl.java
@@ -5,6 +5,7 @@ import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -22,13 +23,18 @@ public class CustomizedReminderRepositoryImpl implements CustomizedReminderRepos
 	@Override
 	public List<Reminder<?>> findByRemindAtBetween(Instant after, Instant before) {
 		Criteria where = where("remindAt").elemMatch(where("").gte(after).lt(before));
-		return unmodifiableList(mongo.find(query(where), Reminder.class));
+		return toGeneric(mongo.find(query(where), Reminder.class));
 	}
 
 	@Override
 	public List<Reminder<?>> findByRemindAtBefore(Instant date) {
 		Criteria where = where("remindAt").elemMatch(where("").lt(date));
-		return unmodifiableList(mongo.find(query(where), Reminder.class));
+		return toGeneric(mongo.find(query(where), Reminder.class));
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private List<Reminder<?>> toGeneric(List<Reminder> find) {
+		return (List<Reminder<?>>) (List<?>) find;
 	}
 
 }
